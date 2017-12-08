99d3529f4297d941df872d4f14f9c12001eae719
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-12-07 23:21:50 -1000

    remove old commented code

--- index.js
+++ index.js
@@ -90,59 +90,6 @@ module.exports = function (mesh, opts) {
     dirs.push(1,-1,1,-1)
     cells.push([j,j+1,j+2],[j,j+2,j+3])
   }
-  /*
-  for (var i = 0; i < mcells.length; i++) {
-    var j = pts.length
-    var c = mcells[i]
-    if (c.length === 2) {
-      pts.push(mesh.positions[c[0]])
-      pts.push(mesh.positions[c[0]])
-      pts.push(mesh.positions[c[1]])
-      pts.push(mesh.positions[c[1]])
-      if (vars) {
-        for (var k = 0; k < vkeys.length; k++) {
-          var vkey = vkeys[k]
-          vars[vkey].push(opts.attributes[vkey][c[0]])
-          vars[vkey].push(opts.attributes[vkey][c[0]])
-          vars[vkey].push(opts.attributes[vkey][c[1]])
-          vars[vkey].push(opts.attributes[vkey][c[1]])
-        }
-      }
-      npts.push(pts[j+2],pts[j+3],pts[j],pts[j+1])
-      dirs.push(1,-1,1,-1)
-      cells.push([j,j+1,j+2],[j,j+2,j+3])
-    } else if (c.length === 3) {
-      pts.push(mesh.positions[c[0]])
-      pts.push(mesh.positions[c[0]])
-      pts.push(mesh.positions[c[1]])
-      pts.push(mesh.positions[c[1]])
-      pts.push(mesh.positions[c[2]])
-      pts.push(mesh.positions[c[2]])
-      if (angles !== null) {
-        // ...
-      }
-      if (vars) {
-        for (var k = 0; k < vkeys.length; k++) {
-          var vkey = vkeys[k]
-          vars[vkey].push(opts.attributes[vkey][c[0]])
-          vars[vkey].push(opts.attributes[vkey][c[0]])
-          vars[vkey].push(opts.attributes[vkey][c[1]])
-          vars[vkey].push(opts.attributes[vkey][c[1]])
-          vars[vkey].push(opts.attributes[vkey][c[2]])
-          vars[vkey].push(opts.attributes[vkey][c[2]])
-        }
-      }
-      npts.push(pts[j+2],pts[j+3],pts[j+4],pts[j+5],pts[j],pts[j+1])
-      dirs.push(1,-1,1,-1,1,-1)
-      cells.push([j,j+1,j+2],[j,j+2,j+3])
-      cells.push([j+2,j+3,j+4],[j+2,j+4,j+5])
-      cells.push([j+4,j+5,j],[j+4,j,j+1])
-    } else {
-      throw new Error('expected a triangle, got '
-        + c.length+'-sided cell')
-    }
-  }
-  */
   return {
     positions: pts,
     cells: cells,
